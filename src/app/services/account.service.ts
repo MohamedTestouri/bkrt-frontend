c9ee91c807674d74e65538cf745356453c766e60
Karim <karimkdaiem@gmail.com>
2024-02-19 19:06:37 +0100

    fix verify user is not null when app start

--- src/app/services/account.service.ts
+++ src/app/services/account.service.ts
@@ -36,11 +36,13 @@ export class AccountService {
   } 
 
   setCurrentUser(user: User) {
-    user.roles = [];
-    const roles = this.getDecodedToken(user.token).role;
-    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
-    localStorage.setItem('user', JSON.stringify(user));
-    this.currentUserSource.next(user);
+    if(user){
+      user.roles = [];
+      const roles = this.getDecodedToken(user.token).role;
+      Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+      localStorage.setItem('user', JSON.stringify(user));
+      this.currentUserSource.next(user);
+    }
   }
 
   logout() {
