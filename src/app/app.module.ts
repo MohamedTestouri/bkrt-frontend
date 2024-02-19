30c21e1019d3d839acfcbd0c5f3eb7119cf15d2f
Karim <karimkdaiem@gmail.com>
2024-02-19 19:07:09 +0100

    add photo carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from './modules/shared.module';
 import { AppRoutingModule } from './app-routing.module';
-import { OverlayModule } from '@angular/cdk/overlay'
-import { CdkMenuModule } from '@angular/cdk/menu'
 
 //Directives
 import { HasRoleDirective } from './directives/has-role.directive';
@@ -22,8 +20,6 @@ import { ServerErrorComponent } from './components/errors/server-error/server-er
 import { ConfirmDialogComponent } from './components/modals/confirm-dialog/confirm-dialog.component';
 import { TextInputComponent } from './components/forms/text-input/text-input.component';
 import { HomeComponent } from './components/home/home.component';
-import { ProfileChoiceComponent } from './components/profile-choice/profile-choice.component';
-import { SideRightComponent } from './components/side-right/side-right.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 
@@ -34,6 +30,9 @@ import { ParentComponent } from './components/Layout/parent/parent.component';
 import { HeaderComponent } from './components/Layout/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ResetPasswordComponent } from './components/auth/reset-password/reset-password.component';
+import { AuthLeftCarouselComponent } from './components/auth/auth-left-carousel/auth-left-carousel.component';
+import { ParentAuthComponent } from './components/auth/parent-auth/parent-auth.component';
+import { ProfileChoiceComponent } from './components/auth/profile-choice/profile-choice.component';
 
 
 @NgModule({
@@ -48,7 +47,6 @@ import { ResetPasswordComponent } from './components/auth/reset-password/reset-p
     HomeComponent,
     LoginComponent,
     ProfileChoiceComponent,
-    SideRightComponent,
 
     BodyComponent,
     SidenavComponent,
@@ -56,6 +54,8 @@ import { ResetPasswordComponent } from './components/auth/reset-password/reset-p
     HeaderComponent,
     DashboardComponent,
     ResetPasswordComponent,
+    AuthLeftCarouselComponent,
+    ParentAuthComponent,
   ],
   imports: [
     BrowserAnimationsModule,
@@ -65,8 +65,6 @@ import { ResetPasswordComponent } from './components/auth/reset-password/reset-p
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    OverlayModule,
-    CdkMenuModule
   ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
