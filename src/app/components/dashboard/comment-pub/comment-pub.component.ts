463182489889498379bb9b434e84608295ef7e1e
Amel <amel.sahbeni.tsi@outlook.fr>
2024-03-06 08:44:02 +0100

    Add Comment-Pub in dashboard

---
+++ src/app/components/dashboard/comment-pub/comment-pub.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comment-pub',
+  templateUrl: './comment-pub.component.html',
+  styleUrls: ['./comment-pub.component.css']
+})
+export class CommentPubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
