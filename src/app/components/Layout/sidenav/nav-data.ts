203a17d742e8d9b01094652fa8822ca83b2762e1
KarimKd <karimkdaiem@gmail.com>
2024-03-24 13:08:35 +0100

    added role to dashboard tabs

--- src/app/components/Layout/sidenav/nav-data.ts
+++ src/app/components/Layout/sidenav/nav-data.ts
@@ -2,36 +2,43 @@ export const navbarData = [
     {
         routeLink: 'dashboard',
         icon: 'fal fa-th-large',
-        label: 'لوحة التحكم'
+        label: 'لوحة التحكم',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'rapports-informations',
         icon: 'fal fa-chart-bar',
-        label: 'التقارير و المعلومات'
+        label: 'التقارير و المعلومات',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'test1',
         icon: 'fal fa-users',
-        label: '  المنتدى الفلاحي'
+        label: '  المنتدى الفلاحي',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'rendezvous',
         icon: 'fal fa-calendar',
-        label: ' المواعيد و الزيارات'
+        label: ' المواعيد و الزيارات',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'subscriptions-packages',
         icon: 'fal fa-files-o',
-        label: 'الحزم'
+        label: 'الحزم',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'home',
         icon: 'fal fa-home',
-        label: 'الرئيسية'
+        label: 'الرئيسية',
+        role: ['Admin', 'Agriculteur']
     },
     {
         routeLink: 'demande',
         icon: 'fal fa-shopping-cart',
-        label: 'الطلبات'
+        label: 'الطلبات',
+        role: ['Admin']
     },
 ];
